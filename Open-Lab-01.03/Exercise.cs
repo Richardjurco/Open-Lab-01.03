d792c0bd0ff32c11fa1225fb2bf9489254712652
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-09-20 09:51:26 +0200

    Update Exercise.cs

--- Open-Lab-01.03/Exercise.cs
+++ Open-Lab-01.03/Exercise.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._03
 {
     public class Exercise
     {
         public int Addition(int number)
+            
         {
-            throw new NotImplementedException();
+             return number + 1;
         }
     }
 }
